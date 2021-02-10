51af27586eae487d02a77506c3dcb41e185d3d45
YaelNeeman26 <yaelneeman10@gmail.com>
2021-02-10 14:32:13 +0200

    Add popup button of add call record.

---
+++ src/components/AddCallRecord.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize'
+import Switch from '@material-ui/core/Switch';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+export default function FormDialog(props) {
+  const [open, setOpen] = React.useState(false);
+
+  // Keep the value of the TextField
+  const [interest, setInterest] = useState('');
+  const [purchased, setPurchased] = useState('');
+  const [support, setSupport] = useState('');
+
+  // Handle on click button
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  
+  const handleClose = () => {
+    setOpen(false);
+    console.log(interest);
+    console.log(purchased);
+    console.log(support);
+    console.log(checked)
+    setChecked(false)
+  };
+
+  // For Switch
+  const [checked, setChecked] = React.useState(false);
+
+  const toggleChecked = () => {
+    setChecked(prev => !prev);
+  };
+  
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>Add Call Record</Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Call Record Details</DialogTitle>
+        <DialogContent>            
+            {/* <TextareaAutosize
+                autoFocus
+                rowsMax	= "2"
+                rowsMin = "2"
+                margin="dense"
+                id="name"
+                label="Interested in ..."
+                type="text"
+                required
+                fullWidth>
+            </TextareaAutosize> */}
+            <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Interested in ..."
+                type="text"
+                fullWidth
+                onChange={(event) => setInterest(event.target.value)}
+            />
+            <TextField
+                margin="dense"
+                id="name"
+                label="Products purchased"
+                type="text"
+                fullWidth
+                multiline
+                onChange={(event) => setPurchased(event.target.value)}
+            />
+            <TextField
+                margin="dense"
+                id="name"
+                onChange={event => setSupport(event.target.value)}
+                label="Support"
+                type="phone"
+                fullWidth
+            />
+            <br></br>
+            <br></br>
+            <DialogContentText>Do return to customer?</DialogContentText>
+            <FormGroup>
+                {/* <FormControlLabel control={<Switch size="small" checked={checked} onChange={toggleChecked} />} label="Small" /> */}
+                <FormControlLabel control={<Switch checked={checked} onChange={toggleChecked} />} label="YES" />
+            </FormGroup>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Submit
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
