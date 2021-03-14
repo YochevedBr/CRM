5b5ed49012d8fb7b2447f4dee4a5196aa26b9f9f
Pessi-L <pessibr@gmail.com>
2021-03-14 19:37:25 +0200

    Write note when the agent havn't done eny calls

--- src/components/CallRecords.js
+++ src/components/CallRecords.js
@@ -15,41 +15,46 @@ function CallRecords() {
         .where("agent_id", "==", localStorage.getItem("agent_id"))
         .get()
         .then((snapshot)=>{
-            console.log(snapshot.docs)
             var callsData = [];
             snapshot.forEach((doc) => {
                 let x = doc.data()
                 x.id = doc.id
-                console.log(x)
                 callsData.push(x)
 
             });    
             callsData = callsData.sort(custom_sort);
-            setCalls(callsData)
+            if (callsData.length === 0){
+                setCalls([0])
+            }
+            else{
+                setCalls(callsData)
+            }
+            
         });
     },[]);
 
     // sort the calls by the dates 
     function custom_sort(a, b) {
-        let x = new Date(b.date).getTime()
-        let y = new Date(a.date).getTime()
         return new Date(b.date).getTime() > new Date(a.date).getTime() ? 1 : -1
     }
 
     return (
         <>
             {
-                calls.length != 0 ?
-                    <div>
-                        <br></br>
-                        <h3 className='font'> Call Records </h3> 
-                        <h5 className='font'> See your activity with our clients </h5> 
-                        <br></br>
-                        <br></br>
-                        <div> 
-                            {calls.map((call, i) => < CallRecord key = { i } call = { call }/>)} 
-                        </div> 
-                    </div>
+                calls.length !== 0 ?
+                    calls[0] === 0 ?
+                        <h1 className='font'>You havn't done any calls yet</h1>
+                    :
+                        <div>
+                            <br></br>
+                            <h3 className='font'> Call Records </h3> 
+                            <h5 className='font'> See your activity with our clients </h5> 
+                            <br></br>
+                            <br></br>
+                            <div> 
+                                {calls.map((call, i) => < CallRecord key = { i } call = { call }/>)} 
+                            </div> 
+                        </div>
                 :
                     <div style={{display: 'flex', justifyContent: 'center'}}>
                         <ReactLoading type='bubbles' color="#000066" />
